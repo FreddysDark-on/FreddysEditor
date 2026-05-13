6dbcb36ffc06f4e9ac589b8998596b3af1205916
FreddysDark-on <96665860+FreddysDark-on@users.noreply.github.com>
2026-05-13 18:30:33 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -4,8 +4,8 @@ function calcularPrecios() {
 
     // --- CONFIGURACIÓN DE PRECIOS POR MINUTO ---
     // Dividimos el precio base original (30 y 80) por los 10 minutos originales
-    const precioMinutoBasico = 3; 
-    const precioMinutoEstandar = 6;
+    const precioMinutoBasico = 2; 
+    const precioMinutoEstandar = 5;
 
     // --- CÁLCULO ---
     let totalBasico = duration * precioMinutoBasico;
